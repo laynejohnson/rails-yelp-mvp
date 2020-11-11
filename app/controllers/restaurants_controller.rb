31576ea02015ac456f37585c1819df23c54dd5d6
Layne Johnson <layne@findum.com>
2020-11-11 14:13:19 -0500

    show

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,27 @@
 class RestaurantsController < ApplicationController
+  # see the list of all restaurants
+  # Add a new restaurant > show
+  # A visitor can see the details of a restaurant, with all the reviews related to the restaurant.
+  # A visitor can add a new review to a restaurant
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+
 end
