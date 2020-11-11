c38570c464fc49a8cb0f27fc94966e25257a2587
Layne Johnson <layne@findum.com>
2020-11-11 12:03:17 -0500

    validation setup

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
+
+  validates :content, presence: true
+  validates :rating, presence: true
+  validates_inclusion_of :rating, :in => 0..5
+  validates :rating, numericality: { only_float: true }
 end
